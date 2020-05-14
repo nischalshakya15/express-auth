8a44208bb8ccdf84a7802dbeafd4b99c8aafbf61
nischalshakya95 <nischalshakya@lftechnology.com>
2020-05-14 21:35:27 +0545

    Setup express with nodejs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Initial setup with express and typescript');
+import express from 'express';
+
+const app: express.Application = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(port, err => {
+    console.log(`Server is listening on ${port}`);
+});
