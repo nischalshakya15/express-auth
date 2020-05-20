0f45fe8891a9e975f3eda43ddb4698d478ce1b82
nischalshakya95 <nischalshakya@lftechnology.com>
2020-05-20 23:28:39 +0545

    Integrate knex

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,21 @@ export const config = {
   app: {
     name: process.env.NAME || 'Server',
     port: process.env.PORT || 8080,
-    host: process.env.HOST || '0.0.0.0',
+    host: process.env.HOST || '0.0.0.0'
   },
+  database: getActiveDatabase(process.env.ACTIVE_DB || 'mysql2')
 };
+
+function getActiveDatabase(db: string) {
+  if (db === 'mysql2') {
+    return {
+      client: db,
+      connection: {
+        user: process.env.DB_MYSQL_USER,
+        password: process.env.DB_MYSQL_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST || '127.0.0.1'
+      }
+    };
+  }
+}
