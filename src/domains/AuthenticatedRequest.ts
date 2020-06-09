9903febcc298384c33b3b508bbbbfc82c747f632
nischalshakya95 <nischalshakya@lftechnology.com>
2020-06-09 09:19:16 +0545

    Supress express Request error with ts-ignore

--- src/domains/AuthenticatedRequest.ts
+++ src/domains/AuthenticatedRequest.ts
@@ -2,6 +2,7 @@ import { Request } from 'express';
 import { Users } from './users/users';
 import { Query } from './query';
 
+// @ts-ignore
 export interface AuthenticatedRequest extends Request {
   users?: Users | any;
   query: Query;
